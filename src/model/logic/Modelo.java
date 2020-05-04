49149b1abe6c1079ff3939dfbd3a090a32414022
Daniel Bernal <da.bernal11@uniandes.edu.co>
2020-05-04 13:57:41 -0500

    Carga en arreglo

--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -65,52 +65,11 @@ public class Modelo{
 		cola = new Queue<Comparendo>();
 		datos = new ArregloDinamico();
 	}
-/**
-	public ArregloDinamico<Comparendo> cargarDatos() {
 
-		JsonReader reader;
-		try {
-			reader = new JsonReader(new FileReader(PATH));
-			JsonElement elem = JsonParser.parseReader(reader);
-			JsonArray e2 = elem.getAsJsonObject().get("features").getAsJsonArray();
-
-
-			SimpleDateFormat parser=new SimpleDateFormat("yyyy/MM/dd");
-
-			for(JsonElement e: e2) {
-				int OBJECTID = e.getAsJsonObject().get("properties").getAsJsonObject().get("OBJECTID").getAsInt();
-
-				String s = e.getAsJsonObject().get("properties").getAsJsonObject().get("FECHA_HORA").getAsString();	
-				Date FECHA_HORA = parser.parse(s); 
-
-				String MEDIO_DETE = e.getAsJsonObject().get("properties").getAsJsonObject().get("MEDIO_DETE").getAsString();
-				String CLASE_VEHI = e.getAsJsonObject().get("properties").getAsJsonObject().get("CLASE_VEHI").getAsString();
-				String TIPO_SERVI = e.getAsJsonObject().get("properties").getAsJsonObject().get("TIPO_SERVI").getAsString();
-				String INFRACCION = e.getAsJsonObject().get("properties").getAsJsonObject().get("INFRACCION").getAsString();
-				String DES_INFRAC = e.getAsJsonObject().get("properties").getAsJsonObject().get("DES_INFRAC").getAsString();	
-				String LOCALIDAD = e.getAsJsonObject().get("properties").getAsJsonObject().get("LOCALIDAD").getAsString();
-
-				double longitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
-						.get(0).getAsDouble();
-
-				double latitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
-						.get(1).getAsDouble();
-
-				Comparendo c = new Comparendo(OBJECTID, FECHA_HORA, DES_INFRAC, MEDIO_DETE, CLASE_VEHI, TIPO_SERVI, INFRACCION, LOCALIDAD, longitud, latitud);
-
-				lista.agregar(c);
-				cola.enqueue(c);
-
-			}
-
-		} catch (FileNotFoundException | ParseException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		return (ArregloDinamico<Comparendo>) datos;
-	}
-*/
-
+	/**
+	 * 
+	 * @return
+	 */
 	public ArregloDinamico<Comparendo> cargarDatos() 
 	{
 		JsonReader reader;
